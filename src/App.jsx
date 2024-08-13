5dc6a0ac853cfd3928f7b17a90d5f6356bad295b
Dakshina Patabandi <kpdakshina@gmail.com>
2024-08-13 17:33:38 +0530

    Display add

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import Sidebar from './Components/Sidebar'
 import Player from './Components/Player'
+import Display from './Components/Display'
 
 const App = () => {
-  return (
-    
+  return (   
     <div className='h-screen bg-black'> 
     <div className='h-[90%] flex'>
       <Sidebar/>
+      <Display/>
     </div>
     <Player/>
     </div>
